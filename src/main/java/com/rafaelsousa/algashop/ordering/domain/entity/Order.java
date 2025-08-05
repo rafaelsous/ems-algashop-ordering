ce17a4e394ac8d30ac044834fb18f60f6528a654
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-08-05 11:31:14 -0300

    EMS-N-02-06.14 - Implementing order aggregate.

---
+++ src/main/java/com/rafaelsousa/algashop/ordering/domain/entity/Order.java
@@ -0,0 +1,207 @@
+package com.rafaelsousa.algashop.ordering.domain.entity;
+
+import com.rafaelsousa.algashop.ordering.domain.valueobject.BillingInfo;
+import com.rafaelsousa.algashop.ordering.domain.valueobject.Money;
+import com.rafaelsousa.algashop.ordering.domain.valueobject.Quantity;
+import com.rafaelsousa.algashop.ordering.domain.valueobject.ShippingInfo;
+import com.rafaelsousa.algashop.ordering.domain.valueobject.id.CustomerId;
+import com.rafaelsousa.algashop.ordering.domain.valueobject.id.OrderId;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+import java.util.Set;
+
+public class Order {
+    private OrderId id;
+    private CustomerId customerId;
+    private Money totalAmount;
+    private Quantity totalItems;
+
+    private Money shippingCost;
+
+    private OffsetDateTime placedAt;
+    private OffsetDateTime paidAt;
+    private OffsetDateTime canceledAt;
+    private OffsetDateTime readyAt;
+
+    private OrderStatus status;
+    private PaymentMethod paymentMethod;
+
+    private BillingInfo billing;
+    private ShippingInfo shipping;
+    private LocalDate expectedDeliveryDate;
+
+    private Set<OrderItem> items;
+
+    public Order(OrderId id, CustomerId customerId, Money totalAmount, Quantity totalItems, Money shippingCost,
+                 OffsetDateTime placedAt, OffsetDateTime paidAt, OffsetDateTime canceledAt, OffsetDateTime readyAt,
+                 OrderStatus status, PaymentMethod paymentMethod, BillingInfo billing, ShippingInfo shipping,
+                 LocalDate expectedDeliveryDate, Set<OrderItem> items) {
+        this.setId(id);
+        this.setCustomerId(customerId);
+        this.setTotalAmount(totalAmount);
+        this.setTotalItems(totalItems);
+        this.setShippingCost(shippingCost);
+        this.setPlacedAt(placedAt);
+        this.setPaidAt(paidAt);
+        this.setCanceledAt(canceledAt);
+        this.setReadyAt(readyAt);
+        this.setStatus(status);
+        this.setPaymentMethod(paymentMethod);
+        this.setBilling(billing);
+        this.setShipping(shipping);
+        this.setExpectedDeliveryDate(expectedDeliveryDate);
+        this.setItems(items);
+    }
+
+    public OrderId id() {
+        return id;
+    }
+
+    public CustomerId customerId() {
+        return customerId;
+    }
+
+    public Money totalAmount() {
+        return totalAmount;
+    }
+
+    public Quantity totalItems() {
+        return totalItems;
+    }
+
+    public Money shippingCost() {
+        return shippingCost;
+    }
+
+    public OffsetDateTime placedAt() {
+        return placedAt;
+    }
+
+    public OffsetDateTime paidAt() {
+        return paidAt;
+    }
+
+    public OffsetDateTime canceledAt() {
+        return canceledAt;
+    }
+
+    public OffsetDateTime readyAt() {
+        return readyAt;
+    }
+
+    public OrderStatus status() {
+        return status;
+    }
+
+    public PaymentMethod paymentMethod() {
+        return paymentMethod;
+    }
+
+    public BillingInfo billing() {
+        return billing;
+    }
+
+    public ShippingInfo shipping() {
+        return shipping;
+    }
+
+    public LocalDate expectedDeliveryDate() {
+        return expectedDeliveryDate;
+    }
+
+    public Set<OrderItem> items() {
+        return items;
+    }
+
+    private void setId(OrderId id) {
+        Objects.requireNonNull(id);
+
+        this.id = id;
+    }
+
+    private void setCustomerId(CustomerId customerId) {
+        Objects.requireNonNull(customerId);
+
+        this.customerId = customerId;
+    }
+
+    private void setTotalAmount(Money totalAmount) {
+        Objects.requireNonNull(totalAmount);
+
+        this.totalAmount = totalAmount;
+    }
+
+    private void setTotalItems(Quantity totalItems) {
+        Objects.requireNonNull(totalItems);
+
+        this.totalItems = totalItems;
+    }
+
+    private void setShippingCost(Money shippingCost) {
+        this.shippingCost = shippingCost;
+    }
+
+    private void setPlacedAt(OffsetDateTime placedAt) {
+        this.placedAt = placedAt;
+    }
+
+    private void setPaidAt(OffsetDateTime paidAt) {
+        this.paidAt = paidAt;
+    }
+
+    private void setCanceledAt(OffsetDateTime canceledAt) {
+        this.canceledAt = canceledAt;
+    }
+
+    private void setReadyAt(OffsetDateTime readyAt) {
+        this.readyAt = readyAt;
+    }
+
+    private void setStatus(OrderStatus status) {
+        Objects.requireNonNull(status);
+
+        this.status = status;
+    }
+
+    private void setPaymentMethod(PaymentMethod paymentMethod) {
+        Objects.requireNonNull(paymentMethod);
+
+        this.paymentMethod = paymentMethod;
+    }
+
+    private void setBilling(BillingInfo billing) {
+        Objects.requireNonNull(billing);
+
+        this.billing = billing;
+    }
+
+    private void setShipping(ShippingInfo shipping) {
+        Objects.requireNonNull(shipping);
+
+        this.shipping = shipping;
+    }
+
+    private void setExpectedDeliveryDate(LocalDate expectedDeliveryDate) {
+        this.expectedDeliveryDate = expectedDeliveryDate;
+    }
+
+    private void setItems(Set<OrderItem> items) {
+        Objects.requireNonNull(items);
+
+        this.items = items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
---
+++ src/main/java/com/rafaelsousa/algashop/ordering/domain/entity/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.rafaelsousa.algashop.ordering.domain.entity;
+
+public enum OrderStatus {
+    DRAFT,
+    PLACED,
+    PAID,
+    READY,
+    CANCELED
+}
