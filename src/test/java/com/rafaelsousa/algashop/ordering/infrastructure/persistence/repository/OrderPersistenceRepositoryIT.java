e1c35c4f8d15faff9890f7ad50da93751ce8eb15
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-08-12 20:20:28 -0300

    EMS-N-02-08.09 - Testing persistence model with integration tests.

--- src/test/java/com/rafaelsousa/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceRepositoryIT.java
+++ src/test/java/com/rafaelsousa/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceRepositoryIT.java
@@ -1,13 +1,46 @@
 package com.rafaelsousa.algashop.ordering.infrastructure.persistence.repository;
 
+import com.rafaelsousa.algashop.ordering.domain.model.utils.IdGenerator;
+import com.rafaelsousa.algashop.ordering.infrastructure.persistence.entity.OrderPersistence;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
+
+@Transactional
 @SpringBootTest
 class OrderPersistenceRepositoryIT {
+    private final OrderPersistenceRepository orderPersistenceRepository;
+
+    @Autowired
+    public OrderPersistenceRepositoryIT(OrderPersistenceRepository orderPersistenceRepository) {
+        this.orderPersistenceRepository = orderPersistenceRepository;
+    }
 
     @Test
-    void shouldRun() {
+    void shouldPersist() {
+        long orderPersistenceId = IdGenerator.generateTSID().toLong();
+        OrderPersistence orderPersistence = OrderPersistence.builder()
+                .id(orderPersistenceId)
+                .customerId(IdGenerator.generateTimeBasedUUID())
+                .totalAmount(BigDecimal.valueOf(1000))
+                .totalItems(2)
+                .status("DRAFT")
+                .paymentMethod("CREDIT_CARD")
+                .build();
 
+        orderPersistenceRepository.saveAndFlush(orderPersistence);
+
+        Assertions.assertThat(orderPersistenceRepository.existsById(orderPersistenceId)).isTrue();
+    }
+
+    @Test
+    void shouldCount() {
+        long ordersCount = orderPersistenceRepository.count();
+
+        Assertions.assertThat(ordersCount).isZero();
     }
 }
