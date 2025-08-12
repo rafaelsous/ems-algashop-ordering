ff21e7b2ba064a24fabb240980adecc0aba8454f
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-08-12 17:46:34 -0300

    EMS-N-02-08.08 - Isolating integration tests execution.

---
+++ src/test/java/com/rafaelsousa/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceRepositoryIT.java
@@ -0,0 +1,13 @@
+package com.rafaelsousa.algashop.ordering.infrastructure.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPersistenceRepositoryIT {
+
+    @Test
+    void shouldRun() {
+
+    }
+}
