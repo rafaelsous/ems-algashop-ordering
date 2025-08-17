15a0d8b539a568933d6ae512f8a63b014e500657
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-08-17 18:50:45 -0300

    EMS-N-02-08.34 - Implementing order listing filtered by customer ID and year using JPQL.

--- src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceRepository.java
+++ src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceRepository.java
@@ -2,11 +2,21 @@ package com.rafaelsousa.algashop.ordering.infrastructure.persistence.repository;
 
 import com.rafaelsousa.algashop.ordering.infrastructure.persistence.entity.OrderPersistence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface OrderPersistenceRepository extends JpaRepository<OrderPersistence, Long> {
-    List<OrderPersistence> findByCustomer_IdAndPlacedAtBetween(UUID customerId, OffsetDateTime start, OffsetDateTime end);
+
+    @Query("""
+            SELECT o
+                    FROM OrderPersistence o
+                    WHERE o.customer.id = :customerId
+                    AND YEAR(o.placedAt) = :year
+            """)
+    List<OrderPersistence> placedByCustomerIdInYear(
+            @Param("customerId") UUID customerId,
+            @Param("year") Integer year);
 }
