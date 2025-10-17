b2199a8b87b04e2a3be3c4dff9dd2205ca0d9858
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-10-17 09:03:24 -0300

    EMS-N-03-04.14 - Implementing contract testing for order not found error.

--- src/test/java/com/rafaelsousa/algashop/ordering/contract/base/OrderBase.java
+++ src/test/java/com/rafaelsousa/algashop/ordering/contract/base/OrderBase.java
@@ -2,6 +2,8 @@ package com.rafaelsousa.algashop.ordering.contract.base;
 
 import com.rafaelsousa.algashop.ordering.application.order.query.OrderDetailOutputTestDataBuilder;
 import com.rafaelsousa.algashop.ordering.application.order.query.OrderQueryService;
+import com.rafaelsousa.algashop.ordering.domain.model.order.OrderId;
+import com.rafaelsousa.algashop.ordering.domain.model.order.OrderNotFoundException;
 import com.rafaelsousa.algashop.ordering.presentation.OrderController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +19,8 @@ import static org.mockito.Mockito.when;
 
 @WebMvcTest(controllers = OrderController.class)
 class OrderBase {
+    private static final String NOT_FOUND_ORDER_ID = "0N8N9TJWPSBWK";
+    public static final String VALID_ORDER_ID = "0N7ZHVJXN94S6";
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -31,8 +35,10 @@ class OrderBase {
 
         RestAssuredMockMvc.enableLoggingOfRequestAndResponseIfValidationFails();
 
-        String id = "0N7ZHVJXN94S6";
-        when(orderQueryService.findById(id))
-                .thenReturn(OrderDetailOutputTestDataBuilder.placedOrder(id).build());
+        when(orderQueryService.findById(VALID_ORDER_ID))
+                .thenReturn(OrderDetailOutputTestDataBuilder.placedOrder(VALID_ORDER_ID).build());
+
+        when(orderQueryService.findById(NOT_FOUND_ORDER_ID))
+                .thenThrow(new OrderNotFoundException(new OrderId(NOT_FOUND_ORDER_ID)));
     }
 }
