eb027fc7c61de66461ddd8fa29844ba6abe10797
Rafael Sousa <rafaelsousa.souza@gmail.com>
2025-12-24 14:43:31 -0300

    EMS-N-03-05.04 - Evaluating the response in detail.

--- src/test/java/com/rafaelsousa/algashop/ordering/presentation/order/OrderControllerIT.java
+++ src/test/java/com/rafaelsousa/algashop/ordering/presentation/order/OrderControllerIT.java
@@ -1,10 +1,13 @@
 package com.rafaelsousa.algashop.ordering.presentation.order;
 
+import com.rafaelsousa.algashop.ordering.domain.model.order.OrderId;
 import com.rafaelsousa.algashop.ordering.infrastructure.persistence.customer.CustomerPersistenceRepository;
 import com.rafaelsousa.algashop.ordering.infrastructure.persistence.entity.CustomerPersistenceTestDataBuilder;
+import com.rafaelsousa.algashop.ordering.infrastructure.persistence.order.OrderPersistenceRepository;
 import com.rafaelsousa.algashop.ordering.utils.AlgaShopResourceUtils;
 import io.restassured.RestAssured;
 import io.restassured.path.json.config.JsonPathConfig;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +19,7 @@ import org.springframework.http.MediaType;
 import java.util.UUID;
 
 import static io.restassured.config.JsonConfig.jsonConfig;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class OrderControllerIT {
@@ -27,6 +31,9 @@ public class OrderControllerIT {
 
     @Autowired
     private CustomerPersistenceRepository customerPersistenceRepository;
+
+    @Autowired
+    private OrderPersistenceRepository orderPersistenceRepository;
 
     private static final UUID VALID_CUSTOMER_ID = UUID.fromString("6e148bd5-47f6-4022-b9da-07cfaa294f7a");
 
@@ -54,17 +61,25 @@ public class OrderControllerIT {
     void shouldCreateOrderWithProduct() {
         String json = AlgaShopResourceUtils.readContent("json/create-order-with-product.json");
 
-        RestAssured
-                .given()
-                    .accept(MediaType.APPLICATION_JSON_VALUE)
-                    .contentType("application/vnd.order-with-product.v1+json")
-                    .body(json)
-                .when()
-                    .post("/api/v1/orders")
-                .then()
+        String createdOrderId = RestAssured
+            .given()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType("application/vnd.order-with-product.v1+json")
+                .body(json)
+            .when()
+                .post("/api/v1/orders")
+            .then()
                 .assertThat()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .statusCode(HttpStatus.CREATED.value());
+                .statusCode(HttpStatus.CREATED.value())
+                .body(
+                        "id", Matchers.not(Matchers.emptyString()),
+                        "customer.id", Matchers.is(VALID_CUSTOMER_ID.toString()))
+            .extract()
+                .jsonPath().getString("id");
+
+        boolean orderExists = orderPersistenceRepository.existsById(new OrderId(createdOrderId).value().toLong());
+        assertThat(orderExists).isTrue();
     }
 
     @Test
