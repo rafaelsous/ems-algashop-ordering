39f9c2fda5d994dc91a9f34a6b2550e40f212d31
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-06-25 12:11:14 -0300

    EMS-N-05-12.01 - Testing security with mocks in integration tests.

--- src/test/java/com/rafaelsousa/algashop/ordering/core/application/AbstractApplicationIT.java
+++ src/test/java/com/rafaelsousa/algashop/ordering/core/application/AbstractApplicationIT.java
@@ -1,10 +1,15 @@
 package com.rafaelsousa.algashop.ordering.core.application;
 
+import com.rafaelsousa.algashop.ordering.core.application.security.SecurityCheckApplicationService;
+import com.rafaelsousa.algashop.ordering.core.domain.model.customer.CustomerTestDataBuilder;
 import com.rafaelsousa.algashop.ordering.infrastructure.config.MockJwtDecoderConfig;
 import com.rafaelsousa.algashop.ordering.infrastructure.config.TestcontainerPostgreSQLConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -13,4 +18,13 @@ import org.springframework.transaction.annotation.Transactional;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class AbstractApplicationIT {
 
+	@MockitoBean
+	protected SecurityCheckApplicationService securityCheckApplicationService;
+
+	@BeforeEach
+	public void preSetup() {
+		Mockito.when(securityCheckApplicationService.isCustomer()).thenReturn(true);
+		Mockito.when(securityCheckApplicationService.getAuthenticatedUserId())
+			.thenReturn(CustomerTestDataBuilder.DEFAULT_CUSTOMER_ID.value());
+	}
 }
