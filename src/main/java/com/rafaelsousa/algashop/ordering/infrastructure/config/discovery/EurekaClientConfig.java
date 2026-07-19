b5acab583a0c5ac4c8efe66913070aafa63d1b73
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-07-18 23:32:12 -0300

    EMS-N-06-01.07 - Setting up Eureka Client for microservces.

---
+++ src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/config/discovery/EurekaClientConfig.java
@@ -0,0 +1,8 @@
+package com.rafaelsousa.algashop.ordering.infrastructure.config.discovery;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {}
