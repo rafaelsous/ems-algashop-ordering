44997c39bb106c495981119c4d883ce24370c829
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-08-16 16:57:57 -0300

    EMS-N-07-03.10 - Implementing deserialization for unknown TypeId.

--- src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/adapters/in/messaging/kafka/product/KafkaProductIntegrationEventListener.java
+++ src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/adapters/in/messaging/kafka/product/KafkaProductIntegrationEventListener.java
@@ -2,7 +2,9 @@ package com.rafaelsousa.algashop.ordering.infrastructure.adapters.in.messaging.k
 
 import com.rafaelsousa.algashop.ordering.core.application.product.event.ProductDelistedIntegrationEvent;
 import com.rafaelsousa.algashop.ordering.core.application.product.event.ProductListedIntegrationEvent;
+
 import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.KafkaHeaders;
@@ -14,15 +16,27 @@ import org.springframework.stereotype.Component;
 @KafkaListener(topics = "product-catalog.product.events")
 public class KafkaProductIntegrationEventListener {
 
-	@KafkaHandler
-	public void handle(ProductListedIntegrationEvent event, @Header(value = KafkaHeaders.RECEIVED_KEY) String messageKey) {
-		log.info("Received {} with key: {} and payload: {}", event.getClass(), messageKey, event);
-		// Handle the event (e.g., update the ordering service's product catalog)
-	}
+    @KafkaHandler(isDefault = true)
+    public void handle(
+            Object event,
+            @Header(value = KafkaHeaders.RECEIVED_KEY) String messageKey,
+            @Header(KafkaHeaders.OFFSET) String messageOffset) {
+        log.info("Event ignored: key: {} and offset: {}", messageKey, messageOffset);
+    }
 
-	@KafkaHandler
-	public void handle(ProductDelistedIntegrationEvent event, @Header(value = KafkaHeaders.RECEIVED_KEY) String messageKey) {
-		log.info("Received {} with key: {} and payload: {}", event.getClass(), messageKey, event);
-		// Handle the event (e.g., update the ordering service's product catalog)
-	}
+    @KafkaHandler
+    public void handle(
+            ProductListedIntegrationEvent event,
+            @Header(value = KafkaHeaders.RECEIVED_KEY) String messageKey) {
+        log.info("Received {} with key: {} and payload: {}", event.getClass(), messageKey, event);
+        // Handle the event (e.g., update the ordering service's product catalog)
+    }
+
+    @KafkaHandler
+    public void handle(
+            ProductDelistedIntegrationEvent event,
+            @Header(value = KafkaHeaders.RECEIVED_KEY) String messageKey) {
+        log.info("Received {} with key: {} and payload: {}", event.getClass(), messageKey, event);
+        // Handle the event (e.g., update the ordering service's product catalog)
+    }
 }
