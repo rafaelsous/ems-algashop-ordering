d4431eafb55a10bcbf2a882517b1a08ea10c5498
Rafael Sousa <rafaelsousa.souza@gmail.com>
2026-09-18 15:00:18 -0300

    EMS-N-07-06.15 - Configures Dead Letter Topic for the consumer.

--- src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/config/kafka/KafkaConfig.java
+++ src/main/java/com/rafaelsousa/algashop/ordering/infrastructure/config/kafka/KafkaConfig.java
@@ -1,16 +1,25 @@
 package com.rafaelsousa.algashop.ordering.infrastructure.config.kafka;
 
 import com.rafaelsousa.algashop.ordering.core.domain.model.DomainException;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.ExponentialBackOff;
 
+import java.time.Duration;
+import java.util.Map;
+
 @Configuration
 public class KafkaConfig {
+	private static final String DLT_PREFIX = "ordering.dlt.";
 
 	@Bean
-	public DefaultErrorHandler defaultErrorHandler() {
+	public DefaultErrorHandler defaultErrorHandler(DeadLetterPublishingRecoverer recoverer) {
 		long interval = 2000L; // 2s entre as tentativas
 		double multiplier = 2;
 		long maxRetries = 3L; // máximo de 2 tentativas
@@ -18,9 +27,36 @@ public class KafkaConfig {
 		ExponentialBackOff exponentialBackOff = new ExponentialBackOff(interval, multiplier);
 		exponentialBackOff.setMaxAttempts(maxRetries);
 
-		DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler(exponentialBackOff);
+		DefaultErrorHandler defaultErrorHandler = new DefaultErrorHandler(recoverer, exponentialBackOff);
 		defaultErrorHandler.addNotRetryableExceptions(DomainException.class);
 
 		return defaultErrorHandler;
+	}
+
+	@Bean
+	public DeadLetterPublishingRecoverer deadLetterPublishingRecoverer(KafkaTemplate<String, Object> kafkaTemplate, AlgaShopMessagingKafkaProperties properties) {
+		DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+			kafkaTemplate,
+			(consumerRecord, _) -> {
+				String dltTopic = DLT_PREFIX + consumerRecord.topic();
+				return new TopicPartition(dltTopic, consumerRecord.partition());
+			});
+
+		recoverer.setLogRecoveryRecord(true);
+		recoverer.setFailIfSendResultIsError(false);
+
+		return recoverer;
+	}
+
+	@Bean
+	public NewTopic productEventsDlt(AlgaShopMessagingKafkaProperties properties) {
+        return TopicBuilder.name(DLT_PREFIX + properties.getProductEventTopicName())
+                .partitions(3)
+                .replicas(3)
+                .configs(Map.of(
+                                "min.insync.replicas", "2",
+                                "retention.ms", String.valueOf(Duration.ofDays(30).toMillis())
+                ))
+                .build();
 	}
 }
